1cecdc447d7c7542cd0c96375591b0d5dac3e8f1
Piyush Dixit <piyushd.bca2022@ssism.org>
2024-05-16 21:44:30 +0530

    justify to center

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
 
     return (
         <>
-            <div className="row d-flex align-items-center py-5" id="about2" data-section='about2' style={{minHeight: "100vh"}}>
+            <div className="row d-flex align-items-center py-5" id="about" data-section='about' style={{minHeight: "100vh"}}>
                 <div className='w-100' style={{height: "50px"}}></div>
                 <motion.h1
                     variants={fadeIn("up",0.5)}
@@ -37,12 +37,12 @@ export const About = () => {
                     // whileInView={"show"}
                     // viewport={{once: true,amount: 0.7}}
                     className='col-12  d-flex flex-column justify-content-center mt-5 mt-md-0 '>
-                    <p className="about2  fs-5 lh-lg  px-lg-4 my-4">Hello, I&apos;m Harsh Dixit, a seasoned Flutter developer with 2.5 years of dedicated experience crafting dynamic and visually stunning cross-platform mobile applications. Throughout my journey in the field, I&apos;ve honed my skills in leveraging Flutter&apos;s robust framework to deliver seamless user experiences across various devices and platforms.</p>
-                    <p className="about2  fs-5 lh-lg  px-lg-4 my-4">
+                    <p className="about  fs-5 lh-lg  px-lg-4 my-4">Hello, I&apos;m Harsh Dixit, a seasoned Flutter developer with 2.5 years of dedicated experience crafting dynamic and visually stunning cross-platform mobile applications. Throughout my journey in the field, I&apos;ve honed my skills in leveraging Flutter&apos;s robust framework to deliver seamless user experiences across various devices and platforms.</p>
+                    <p className="about  fs-5 lh-lg  px-lg-4 my-4">
                         Driven by a passion for innovation and a keen eye for detail, I thrive on turning ideas into polished, functional realities. My portfolio showcases a diverse range of projects, each reflecting my commitment to excellence and my ability to adapt to evolving industry trends.</p>
-                    <p className="about2  fs-5 lh-lg  px-lg-4 my-4">
+                    <p className="about  fs-5 lh-lg  px-lg-4 my-4">
                         From conceptualization to deployment, I specialize in translating client visions into intuitive and feature-rich applications that resonate with users. With a strong foundation in both front-end and back-end development, I bring a comprehensive approach to every project, ensuring scalability, performance, and user satisfaction.</p>
-                    <p className="about2  fs-5 lh-lg  px-lg-4 my-4">
+                    <p className="about  fs-5 lh-lg  px-lg-4 my-4">
                         Let&apos;s collaborate to bring your next app idea to life with the magic of Flutter!</p>
                 </motion.div>
             </div>
