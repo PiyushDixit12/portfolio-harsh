1909678677d4bea70b40dbf9e1a41a47bcd3b914
Piyush Dixit <piyush.dixit_hri@freighttiger.com>
2025-05-06 22:38:02 +0530

    changed skill formate and add new skils of frontend and backend

--- src/pages/skill/Skills.jsx
+++ src/pages/skill/Skills.jsx
@@ -1,10 +1,10 @@
-import {FaHtml5,FaReact} from "react-icons/fa";
+import {FaHtml5,FaNode,FaNodeJs,FaReact} from "react-icons/fa";
 import {FaCss3Alt} from "react-icons/fa";
 import {TbBrandBootstrap} from "react-icons/tb";
 import {FaGitAlt} from "react-icons/fa";
 import {BiLogoFlutter} from "react-icons/bi";
 import {TbBrandVscode} from "react-icons/tb";
-import {SiFirebase} from "react-icons/si";
+import {SiExpress, SiFirebase} from "react-icons/si";
 import {SkillCard} from "./SkillCard";
 import {motion} from "framer-motion";
 import dart from '../../assets/dart language.svg'
@@ -13,7 +13,38 @@ import {SiStrapi} from "react-icons/si";
 import {DiJavascript} from "react-icons/di";
 import {IoLogoGooglePlaystore} from "react-icons/io5";
 import {FaAppStoreIos} from "react-icons/fa6";
+import { GrMysql } from "react-icons/gr";
 const skillsData = [
+    {
+        languageName: "Dart",
+        languageIcon: <img src={dart} width={"150px"} alt="" />
+
+    },
+    {
+        languageName: "Flutter",
+        languageIcon: <BiLogoFlutter size={"150px"} color='rgb(21,181,114)' />
+    },
+    {
+        languageName: "Git",
+        languageIcon: <FaGitAlt size={"150px"} color='rgb(21,181,114)' />
+    },
+    {
+        languageName: "Vs Code",
+        languageIcon: <TbBrandVscode size={"150px"} color='rgb(21,181,114)' />
+
+    },
+    {
+        languageName: "App Store",
+        languageIcon: <FaAppStoreIos size={"150px"} color='rgb(21,181,114)' />
+    }
+    ,{
+        languageName: "Play Store",
+        languageIcon: <IoLogoGooglePlaystore size={"150px"} color='rgb(21,181,114)' />
+    },
+    {
+        languageName: "Firebase",
+        languageIcon: <SiFirebase size={"150px"} color='rgb(21,181,114)' />
+    },
     {
         languageName: "HTML",
         languageIcon: <FaHtml5 size={"150px"} color='rgb(21,181,114)' />
@@ -28,39 +59,25 @@ const skillsData = [
     },{
         languageName: "Java Script",
         languageIcon: <DiJavascript size={"150px"} color='rgb(21,181,114)' />
-    },{
+    }
+    , {
+        languageName: "Node",
+        languageIcon: <FaNodeJs size={"150px"} color='rgb(21,181,114)' />
+    }, {
+        languageName: "Express",
+        languageIcon: <SiExpress size={"150px"} color='rgb(21,181,114)' />
+    },
+    {
+        languageName: "MySQL",
+        languageIcon: <GrMysql size={"150px"} color='rgb(21,181,114)' />
+    },
+    {
         languageName: "React",
         languageIcon: <FaReact size={"150px"} color='rgb(21,181,114)' />
     },
     {
-        languageName: "Vs Code",
-        languageIcon: <TbBrandVscode size={"150px"} color='rgb(21,181,114)' />
-
-    },{
-        languageName: "Git",
-        languageIcon: <FaGitAlt size={"150px"} color='rgb(21,181,114)' />
-    },
-    {
-        languageName: "Dart",
-        languageIcon: <img src={dart} width={"150px"} alt="" />
-
-    },{
-        languageName: "Flutter",
-        languageIcon: <BiLogoFlutter size={"150px"} color='rgb(21,181,114)' />
-    }
-    ,{
-        languageName: "Firebase",
-        languageIcon: <SiFirebase size={"150px"} color='rgb(21,181,114)' />
-    },{
         languageName: "Strapi Js",
         languageIcon: <SiStrapi size={"150px"} color='rgb(21,181,114)' />
-    },{
-        languageName: "App Store",
-        languageIcon: <FaAppStoreIos size={"150px"} color='rgb(21,181,114)' />
-    }
-    ,{
-        languageName: "Play Store",
-        languageIcon: <IoLogoGooglePlaystore size={"150px"} color='rgb(21,181,114)' />
     },
 ];
 export const Skills = () => {
